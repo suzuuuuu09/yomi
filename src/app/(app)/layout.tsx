378eba7c285af6c938c2152c3600e25d8eb27c32
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2026-03-17 13:17:52 +0900

    ✨OGP

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -17,7 +17,22 @@ const firaSans = Fira_Sans({
 
 export const metadata: Metadata = {
   title: "YOMi - 読書を宇宙に",
-  description: "",
+  description:
+    "YOMiは一冊の本を一つの「星」に見立てて記録し、自分だけの星空を作る読書管理アプリです。",
+  openGraph: {
+    type: "website",
+    title: "YOMi - 読書を宇宙に",
+    description:
+      "YOMiは一冊の本を一つの「星」に見立てて記録し、自分だけの星空を作る読書管理アプリです。",
+    url: "https://yomi.suzuuuuu09.com",
+    siteName: "YOMi",
+    images: {
+      url: "/ogp.png",
+      width: 1200,
+      height: 630,
+      type: "image/png",
+    },
+  },
 };
 
 export default function AppLayout({
